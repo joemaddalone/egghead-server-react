e641ff07d0a7f2e6a5b45a8fde4437a76063c63a
Joe Maddalone <joe@insert-title.com>
2016-04-09 10:09:18 -0500

    Update to React v15

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 var Component = React.createFactory(require('./Component'));
 
 
@@ -8,7 +9,8 @@ var app = express();
 function home(req, res){
   var msg = req.params.msg || 'Hello';
   var comp = Component({msg:msg});
-  res.send(React.renderToStaticMarkup(comp));
+  //res.send(React.renderToStaticMarkup(comp));
+  res.send(ReactDOMServer.renderToStaticMarkup(comp))
 }
 
 app.get('', home);
